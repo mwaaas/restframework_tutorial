47f23ec960f0f500b918ff3893f4180755784f46
mwas <francismwangi152@gmail.com>
2014-11-11 12:56:39 +0300

    refactored

--- example/views.py
+++ example/views.py
@@ -1,8 +1,19 @@
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 
 from django.contrib.auth.models import User, Group
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import viewsets
 from example.serializer import UserSerializer, GroupSerializer
+from rest_framework.decorators import api_view
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+from .models import Snippet
+from .serializer import SnippetSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework import generics
 
 
 class UserViewSet(viewsets.ModelViewSet):
@@ -19,3 +30,13 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
--- restframework_tutorial/urls.py
+++ restframework_tutorial/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from rest_framework import routers
 from example import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -14,5 +15,15 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include(router.urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-)
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    )
+
+other_url= [
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)$', views.SnippetDetail.as_view())
+]
+
+urlpatterns += format_suffix_patterns(other_url)
+
+
+
